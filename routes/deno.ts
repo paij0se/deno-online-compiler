ae555e7d8e14c08369fdd67195f04f45d6de62af
pai <hachikopolarcito321@gmail.com>
2021-07-04 14:59:19 -0500

    now use the api

--- routes/deno.ts
+++ routes/deno.ts
@@ -1,30 +1,20 @@
-import { Router, json } from "express";
-import { exec } from "child_process";
-import { writeFile } from "fs";
+import { Router } from "express";
+import axios from "axios";
 
 const router = Router();
-const deno =
-  "./deno run --allow-net --no-check  execute.ts";
 
-const regex =
-  /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
 router.post("/code", async (req, res) => {
   const { code } = req.body;
   console.log(code);
 
-  writeFile("execute.ts", code, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
-  exec(deno, { timeout: 1000 }, (_, stdout, stderr) => {
-    let out = (stdout || stderr).replace(regex, "");
-    console.log(out);
-    res.json({
-      out: out,
-    });
-  });
+  axios
+    .post("https://api-deno-compiler.elpanajose.repl.co/code", {
+      code: code,
+    })
+    .then((r) => {
+      console.log(r.data.out);
+      res.json({ out: r.data.out });
+    }).catch((err) => err ? res.json({ out: "to many requests" }) : null);
 });
 
 module.exports = router;
