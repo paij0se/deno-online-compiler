c638e2f074c5bfac5707b94ae11421730dce0258
pai <hachikopolarcito321@gmail.com>
2021-07-06 00:56:18 -0500

    add the response api time

--- routes/status.go
+++ routes/status.go
@@ -5,16 +5,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	resp, err := http.Get("https://api-deno-compiler.herokuapp.com/code")
 	if err != nil {
 		log.Fatalln("ERROR 400 SENDING THE REQUEST TO THE API", err)
 	}
+	duration := time.Since(start)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("HTTP RESPONSE 200, ALL GOOD", string(body))
+	fmt.Printf("API Response Time: %d%s\n", duration.Milliseconds(), "ms")
+	fmt.Println("Response 200 All Good")
+	fmt.Println(string(body))
 }
